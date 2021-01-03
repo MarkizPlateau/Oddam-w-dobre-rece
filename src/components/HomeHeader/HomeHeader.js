0fe2f4043026cbb1ec3f1441735c19f94c8cb4e3
unknown <s.marek.adamczyk@gmail.com>
2021-01-03 22:59:16 +0100

    signin layout

--- src/components/HomeHeader/HomeHeader.js
+++ src/components/HomeHeader/HomeHeader.js
@@ -1,5 +1,4 @@
 import React from "react";
-import HomeNavigation from "../HomeNavigation/HomeNavigation";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../../constans/routes";
 import { ReactComponent as Decoration} from "../../assets/Decoration.svg";
@@ -8,7 +7,6 @@ import { ReactComponent as Decoration} from "../../assets/Decoration.svg";
 const HomeHeader = () => {
     return (
         <>
-                <HomeNavigation />
                 <section className={"header-section"}>
                     <h1 className={"header-section-title"}>Zacznij pomagać! <br/> Oddaj niechciane rzeczy w zaufane ręce</h1>
                     <Decoration className={"header-section-decoration"} />
